f3e49f126244e85dbab23fb0805686bec8d84d62
Samuel Raub <samuel.raub@gmail.com>
2019-10-01 20:24:09 +0200

    random

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleChange = e => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('photo', file);
+    console.log(file);
+    console.log(formData);
+    fetch('/api', { method: 'POST', body: formData })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        type="file"
+        accept="image/png, image/jpeg"
+        onChange={handleChange}
+      />
+    </>
   );
 }
 
