d3d7399c7e8ad4d731e63cde76b8b706be875b9f
Samuel Raub <samuel.raub@gmail.com>
2019-10-01 23:29:32 +0200

    Add age input.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
 import './App.css';
 
+import React, { useRef, useState } from 'react';
+
 function App() {
+  const fileRef = useRef(null);
+
+  const [formState, setFormState] = useState({ age: '0', photo: '' });
   const handleChange = e => {
-    const file = e.target.files[0];
+    const target = e.target;
+
+    const name = target.name;
+    const value = target.value;
+
+    setFormState(prev => ({
+      ...prev,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const file = fileRef.current.files[0];
     const formData = new FormData();
     formData.append('photo', file);
-    console.log(file);
-    console.log(formData);
+    formData.append('age', formState.age);
     fetch('/api', { method: 'POST', body: formData })
       .then(res => console.log(res))
       .catch(err => console.log(err));
@@ -15,11 +31,21 @@ function App() {
 
   return (
     <>
-      <input
-        type="file"
-        accept="image/png, image/jpeg"
-        onChange={handleChange}
-      />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          name="age"
+          value={formState.age}
+          onChange={handleChange}
+        />
+        <input
+          ref={fileRef}
+          type="file"
+          accept="image/png, image/jpeg"
+          name="photo"
+        />
+        <input type="submit" />
+      </form>
     </>
   );
 }
