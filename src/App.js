afdd9722755a30fd9a266e6793d48329fea296a3
Samuel Raub <samuel.raub@gmail.com>
2019-10-08 23:15:32 +0200

    Styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
+import styled from 'styled-components';
+import Slider from '@material-ui/core/Slider';
+import Submit from './Submit';
+import Wrapper from './Wrapper';
+
+import './index.css';
 
 import React, { useRef, useState } from 'react';
+
+const Upload = styled(Submit)`
+  width: 100%;
+  height: 15rem;
+  margin: 2rem 0;
+  color: white;
+  background: none;
+`;
+
+const AgeLabel = styled.p`
+  color: white;
+  font-size: 3rem;
+`;
 
 function App() {
   const fileRef = useRef(null);
 
   const [formState, setFormState] = useState({ age: '0', photo: '' });
+
+  const [error, setError] = useState('');
+
+  const [success, setSuccess] = useState(false);
+
   const handleChange = e => {
     const target = e.target;
 
@@ -20,32 +44,72 @@ function App() {
 
   const handleSubmit = e => {
     e.preventDefault();
+    if (success) return;
+
     const file = fileRef.current.files[0];
+
+    if (!file) {
+      setError('NEED PHOTO');
+      return;
+    }
+
     const formData = new FormData();
     formData.append('photo', file);
     formData.append('age', formState.age);
     fetch('/api', { method: 'POST', body: formData })
-      .then(res => console.log(res))
-      .catch(err => console.log(err));
+      .then(res => {
+        console.log(res);
+        if (res.status !== 200) {
+          setError('OOPS');
+        } else {
+          setSuccess(true);
+        }
+      })
+      .catch(err => {
+        setError('OOPS');
+        console.log(err);
+      });
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="number"
-          name="age"
-          value={formState.age}
-          onChange={handleChange}
-        />
-        <input
-          ref={fileRef}
-          type="file"
-          accept="image/png, image/jpeg"
-          name="photo"
-        />
-        <input type="submit" />
-      </form>
+      <Wrapper>
+        <form onSubmit={handleSubmit}>
+          <AgeLabel>ENTER YOUR AGE</AgeLabel>
+          <Slider
+            defaultValue={30}
+            aria-labelledby="discrete-slider"
+            valueLabelDisplay="auto"
+            step={1}
+            min={1}
+            max={99}
+            onChange={(e, val) => setFormState(prev => ({ ...prev, age: val }))}
+          />
+          <p>
+            <Upload
+              as="button"
+              onClick={e => {
+                e.preventDefault();
+                setError('');
+                fileRef.current.click();
+              }}
+            >
+              TAKE PHOTO
+            </Upload>
+            <input
+              style={{ display: 'none' }}
+              ref={fileRef}
+              type="file"
+              accept="image/png, image/jpeg"
+              name="photo"
+            />
+            <Submit
+              type="submit"
+              value={error ? error : success ? 'THANKS' : 'SUBMIT'}
+            />
+          </p>
+        </form>
+      </Wrapper>
     </>
   );
 }
---
+++ src/Submit.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export default styled.input`
+  width: 100%;
+  border: 3px solid white;
+  font-family: Helvetica, Arial, sans-serif;
+  font-size: 2.5rem;
+  padding: 2rem;
+  outline: none;
+  &:active {
+    color: black;
+    background: white;
+  }
+`;
